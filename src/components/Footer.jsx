3d080ff7dc3cd0185a62ca5200f1db9152326937
adzaky <muhadlim17@gmail.com>
2024-10-02 23:09:07 +0700

    feat: full width footer in medium to wider devices

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="mx-auto rounded-2xl border p-6">
+    <footer className="mx-auto w-full rounded-2xl border p-6 md:text-center">
       <p className="text-xs">Made with Next.js, shadcnUI, and TailwindCSS, using my last 3 brain cells.</p>
     </footer>
   );
