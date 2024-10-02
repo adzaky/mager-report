555dee810f49bdb69e201d6604719a941d83fc96
adzaky <muhadlim17@gmail.com>
2024-10-02 22:43:38 +0700

    feat: set gap for max width medium devices

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import GenerateReport from "@/components/GenerateReport";
 
 export default function Home() {
   return (
-    <main className="mx-auto flex min-h-screen max-w-screen-lg flex-col p-4 md:items-center md:justify-evenly">
+    <main className="mx-auto flex min-h-screen max-w-screen-lg flex-col p-4 max-md:gap-4 md:items-center md:justify-evenly">
       <GenerateReport />
       <Footer />
     </main>
