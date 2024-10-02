331dd13ba455a85fa32a63f069f20594803bd3a2
adzaky <muhadlim17@gmail.com>
2024-10-02 22:41:41 +0700

    feat: only set align and justify in medium and wider devices

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import GenerateReport from "@/components/GenerateReport";
 
 export default function Home() {
   return (
-    <main className="mx-auto flex min-h-screen max-w-screen-lg flex-col items-center justify-evenly p-4">
+    <main className="mx-auto flex min-h-screen max-w-screen-lg flex-col p-4 md:items-center md:justify-evenly">
       <GenerateReport />
       <Footer />
     </main>
