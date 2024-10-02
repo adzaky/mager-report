ad2b774eea1d0f6673b3b63cdec7945838c94d68
adzaky <muhadlim17@gmail.com>
2024-10-02 22:49:24 +0700

    feat: improve footer layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="mx-auto">
+    <footer className="mx-auto rounded-2xl border p-6">
       <p className="text-xs">Made with Next.js, shadcnUI, and TailwindCSS, using my last 3 brain cells.</p>
     </footer>
   );
