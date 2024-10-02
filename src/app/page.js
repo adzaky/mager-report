82bcedafd3add90f081b151c7e4b644d4e68e925
adzaky <muhadlim17@gmail.com>
2024-10-02 22:26:26 +0700

    feat: create footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
+import Footer from "@/components/Footer";
 import GenerateReport from "@/components/GenerateReport";
 
 export default function Home() {
   return (
-    <main>
-      <div className="mx-auto flex min-h-screen max-w-screen-lg items-center justify-center p-4">
-        <GenerateReport />
-      </div>
+    <main className="mx-auto flex min-h-screen max-w-screen-lg flex-col items-center justify-evenly p-4">
+      <GenerateReport />
+      <Footer />
     </main>
   );
 }
