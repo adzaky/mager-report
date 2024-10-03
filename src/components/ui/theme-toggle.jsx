b43604788ecd401b2d02f6318d131e5666132635
adzaky <muhadlim17@gmail.com>
2024-10-03 14:23:47 +0700

    feat: use dropdown menu for theme toggle

--- src/components/ui/theme-toggle.jsx
+++ src/components/ui/theme-toggle.jsx
@@ -1,20 +1,45 @@
 "use client";
-import { Moon, Sun } from "lucide-react";
+
+import * as React from "react";
+import { Laptop, Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
-import { Button } from "./button";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const ThemeToggle = () => {
-  const { setTheme, theme } = useTheme();
+  const { setTheme } = useTheme();
 
   return (
-    <Button
-      isIconOnly
-      variant="outline"
-      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-      className="ml-auto px-3 py-2"
-    >
-      {theme === "light" ? <Sun className="size-4" /> : <Moon className="size-4" />}
-    </Button>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon" className="ml-auto">
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          <Sun className="mr-2 size-4" />
+          <span>Light</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          <Moon className="mr-2 size-4" />
+          <span>Dark</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          <Laptop className="mr-2 size-4" />
+          <span>System</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
+
 export default ThemeToggle;
