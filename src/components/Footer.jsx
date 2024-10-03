3dd149d71752bc51b7af4b1329093adcc5c73a8a
adzaky <muhadlim17@gmail.com>
2024-10-03 10:42:49 +0700

    feat: adjust footer text and padding size

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <footer className="mx-auto w-full rounded-2xl border p-6 md:text-center">
-      <p className="text-xs">Made with Next.js, shadcnUI, and TailwindCSS, using my last 3 brain cells.</p>
+    <footer className="mx-auto w-full rounded-2xl border p-6 md:p-4 md:text-center">
+      <p className="max-md:text-xs">Made with Next.js, shadcnUI, and TailwindCSS, using my last 3 brain cells.</p>
     </footer>
   );
 };
