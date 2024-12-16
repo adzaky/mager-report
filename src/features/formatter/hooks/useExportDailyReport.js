306d7d0c2ea0a02890f7eb927b8a8abeca333d76
adzaky <muhadlim17@gmail.com>
2024-12-16 13:46:38 +0700

    feat: add null fallback export daily report task

--- src/features/formatter/hooks/useExportDailyReport.js
+++ src/features/formatter/hooks/useExportDailyReport.js
@@ -13,14 +13,21 @@ export const useExportDailyReport = () => {
       const res = await dailyReportService.getMonthlyReports();
       if (res?.status === "success") {
         const exportData = res?.data?.map((report, index) => {
-          const taskDescriptions = report?.tasks?.map((task) => task.description);
-          const taskTimestamps = report?.tasks?.map((task) => `${task.startTime} - ${task.endTime}`);
-          const formattedTasks = taskDescriptions.map((description, i) => `${description} (${taskTimestamps[i]})`);
+          const taskDescriptions = report?.tasks?.map((task) => task.description) || [];
+          const taskTimestamps = report?.tasks?.map((task) => `${task.startTime} - ${task.endTime}`) || [];
+          const formattedTasks = taskDescriptions.length
+            ? taskDescriptions.map((description, i) => `${description} (${taskTimestamps[i]})`)
+            : ["No tasks available"];
 
           return {
             No: index + 1,
             "Report Status": report?.reportStatus,
-            "Report Date": new Intl.DateTimeFormat('id-ID', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }).format(new Date(report?.updated_at ?? report?.created_at)),
+            "Report Date": new Intl.DateTimeFormat("id-ID", {
+              weekday: "long",
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            }).format(new Date(report?.updated_at ?? report?.created_at)),
             Tasks: formattedTasks,
           };
         });
